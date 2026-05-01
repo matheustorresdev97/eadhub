e15d8db7a735d14b8a1cda9b914a270f54010122
matheustorresdev97 <matheustorresdev@gmail.com>
2026-05-01 12:28:26 -0300

    feat: implementar o Spring Cloud Config Server e migrar as configurações do aplicativo para um repositório remoto.

---
+++ config-server/src/test/java/com/matheustorres/eadhub/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheustorres.eadhub.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
